4c0aecfa8c8c2c3400d3aba7b23ff01ebd4ce3e4
marocchino <marocchino@users.noreply.github.com>
2011-06-02 18:23:05 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+# xcode check 
+# install homebrew
+ruby -e "$(curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb)"
+
+# install git
+brew install git
+
+# install curl
+brew install curl
+bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+
+# install npm
+brew install node
+curl http://npmjs.org/install.sh | sh
+
+# install etc
+brew install mvim zsh
