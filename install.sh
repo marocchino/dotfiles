57c9db8c690d067230d455bfd38d93a5fbaf6e0b
marocchino <marocchino@users.noreply.github.com>
2011-09-30 20:32:20 +0900

    remove install stubs

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 # xcode check
 # install homebrew
-ruby -e "$(curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb)"
+#ruby -e "$(curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb)"
 
 # install git
-brew install git
+#brew install git
 
 # install curl
-brew install curl
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+#brew install curl
+#bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 
 # install npm
-brew install node
-curl http://npmjs.org/install.sh | sh
+#brew install node
+#curl http://npmjs.org/install.sh | sh
 
 # install etc
-brew install macvim zsh
-npm install -g coffee-script
-
+#brew install macvim zsh
+#npm install -g coffee-script
 
 ln -s vim ~/.vim
 ln -s vimrc ~/.vimrc
