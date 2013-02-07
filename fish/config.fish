1d3091250930ea5dfe91e49997046c68d38dc2ab
marocchino <marocchino@users.noreply.github.com>
2013-02-07 23:57:27 +0900

    change rvm to rbenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
-rvm > /dev/null
-set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/local/sbin $PATH
-set -x PATH /usr/local/share/npm/bin $PATH
+set -x PATH $HOME/.rbenv/shims $HOME/.rbenv/bin /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin $PATH
+set -x NODE_PATH /usr/local/lib/node_modules
+rbenv rehash >/dev/null ^&1
 function go
   git checkout $argv
 end
