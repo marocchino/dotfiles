21680412efb8772dc71df33f10e8b3813e2b52c5
marocchino <marocchino@users.noreply.github.com>
2013-04-14 12:38:06 +0900

    split path diffrent line

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
-set -x PATH $HOME/.rbenv/shims $HOME/.rbenv/bin /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin $PATH
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/share/npm/bin $PATH
 set -x NODE_PATH /usr/local/lib/node_modules
+set -x PATH $HOME/.rbenv/shims $HOME/.rbenv/bin /Users/marocchino/.rbenv/versions/2.0.0-p0/bin $PATH
 rbenv rehash >/dev/null ^&1
 function go
   git checkout $argv
