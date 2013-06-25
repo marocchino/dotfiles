ebd14614fea614f1f320674995d302a582467c96
marocchino <marocchino@users.noreply.github.com>
2013-06-26 08:54:17 +0900

    add tmux

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,8 @@ set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/share/npm/bin $PATH
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH $HOME/.rbenv/shims $PATH
-set -x PATH $HOME/.rbenv/bin $PATH
+set -x PATH $HOME/.rbenv/shims $PATH
+set -x PATH $HOME/bin $PATH
 rbenv rehash >/dev/null ^&1
 function go
   git checkout $argv
