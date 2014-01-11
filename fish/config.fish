355b992f498e548a6024a8fd97a32a99bee49613
marocchino <marocchino@users.noreply.github.com>
2014-01-11 20:39:29 +0900

    Fix typo

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
 set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/sbin $PATH
 set -x PATH /usr/local/share/npm/bin $PATH
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH $HOME/.rbenv/shims $PATH
-set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $HOME/bin $PATH
-set -x DYLD_LIBRARY_PATH $HOME/Dropbox/Development/dotfiles/instantclient_11_2
+set -x ANDROID_HOME /usr/local/opt/android-sdk
+set -x PATH $PATH $ANDROID_HOME/tools
+set -x PATH $PATH $ANDROID_HOME/platform-tools
+set -x PATH $PATH $ANDROID_HOME/build-tools
+set -x PATH $PATH /Applications/Kindle\ Previewer.app/Contents/MacOS/lib
 rbenv rehash >/dev/null ^&1
 . ~/.config/fish/profile.fish
