23194ede4393b99611d2bf94ef1867664fe213e8
marocchino <marocchino@users.noreply.github.com>
2015-01-22 09:40:25 +0900

    Add boot2docker setting

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,6 @@ set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $PATH /Applications/Kindle\ Previewer.app/Contents/MacOS/lib
 rbenv rehash >/dev/null ^&1
 . ~/.config/fish/profile.fish
+set -x DOCKER_HOST tcp://boot2docker:2376
+set -x DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
