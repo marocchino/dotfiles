53724152b9f57d0f609b47a889a38996c0376574
marocchino <marocchino@users.noreply.github.com>
2015-02-08 02:13:50 +0900

    Add command exist check

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,6 +1,16 @@
 function update
-  brew update
-  brew upgrade
-  brew cleanup
-  sudo motion update
+  if type brew
+    echo updating brew
+    brew update
+    brew upgrade
+    brew cleanup
+  end
+  if type apt-get
+    echo updating apt-get
+    sudo apt-get update -y
+  end
+  if type motion
+    echo updating motion
+    sudo motion update
+  end
 end
