96550810e9124aaaf72e04dc4d0799de52615c95
marocchino <marocchino@users.noreply.github.com>
2015-02-21 02:41:01 +0900

    Improve scripts

--- fish/functions/pmvim.fish
+++ fish/functions/pmvim.fish
@@ -1,11 +1,7 @@
 function pmvim
-  if test (count $argv) = 0
-    set peco_flags --layout=bottom-up
+  find * -type f | peco $peco_flags | read filename
+  if test (count $filename) = 0
   else
-    set peco_flags --layout=bottom-up --query "$argv"
+    mvim $filename
   end
-  set pwd (pwd)"/"
-  echo $pwd
-  ls ** | peco $peco_flags | read filename
-  mvim $filename
 end
