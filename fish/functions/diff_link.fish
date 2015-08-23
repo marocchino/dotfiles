b65725b3e52a0a8a08d74b767b5642b80a94cfb2
marocchino <marocchino@users.noreply.github.com>
2015-08-23 10:19:48 +0900

    Add diff_link command

---
+++ fish/functions/diff_link.fish
@@ -0,0 +1,10 @@
+function diff_link
+  if test (git remote -v | wc -l) -eq 2
+    set GITHUB_URL (git remote -v | awk '{ print $2 }' | head -1)
+  else
+    set GITHUB_URL (git remote -v | grep -v origin | awk '{ print $2 }' | head -1)
+  end
+
+  set GITHUB_URL (echo $GITHUB_URL | sed -e 's/\.git//g' | sed -e 's/git@github.com:/https:\/\/github.com\//')
+  open $GITHUB_URL/compare/$argv[1]
+end
