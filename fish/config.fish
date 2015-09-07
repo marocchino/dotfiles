c6ef25dcbb0827b01503c2e7347404fd7efe0544
marocchino <marocchino@users.noreply.github.com>
2015-09-07 11:07:22 +0900

    Remove docker settings

--- fish/config.fish
+++ fish/config.fish
@@ -5,9 +5,6 @@ set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $PATH /Applications/Kindle\ Previewer.app/Contents/MacOS/lib
 rbenv rehash >/dev/null ^&1
-set -x DOCKER_HOST tcp://boot2docker:2376
-set -x DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
-set -x DOCKER_TLS_VERIFY 1
 fish_user_key_bindings
 source ~/.config/fish/profile.fish
 
