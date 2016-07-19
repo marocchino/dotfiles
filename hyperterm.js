0fc8411ace512adbcbc9e4cb7aae854e4d518d12
marocchino <marocchino@users.noreply.github.com>
2016-07-19 17:45:13 +0900

    Add 1password plugin

--- hyperterm.js
+++ hyperterm.js
@@ -55,7 +55,7 @@ module.exports = {
   //   `hypersolar`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperpower'],
+  plugins: ['hyperpower', 'hyperterm-1password'],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
