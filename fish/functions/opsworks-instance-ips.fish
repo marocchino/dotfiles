843be896cd56e5c36864e4f48b234f5fa73b31e3
marocchino <marocchino@users.noreply.github.com>
2016-08-17 05:50:57 +0900

    Remove useless

--- fish/functions/opsworks-instance-ips.fish
+++
@@ -1,5 +0,0 @@
-function opsworks-instance-ips
-  for i in $argv
-    aws opsworks describe-instances --region=us-east-1 --stack-id=$i | python -c "import json,sys;obj=json.load(sys.stdin);print '\n'.join(' : '.join([stack['Hostname'], stack['PrivateIp']]) for stack in obj['Instances'])"
-  end
-end
--- fish/functions/opsworks-stack-ids.fish
+++
@@ -1,3 +0,0 @@
-function opsworks-stack-ids
-  aws opsworks describe-stacks --region=us-east-1 | python -c "import json,sys;obj=json.load(sys.stdin);print '\n'.join(' : '.join([stack['StackId'], stack['Name']]) for stack in obj['Stacks'])"
-end
