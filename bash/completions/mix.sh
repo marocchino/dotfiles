f6ba8801e23a001bb32082ea910aa6f87a50997a
marocchino <marocchino@users.noreply.github.com>
2021-05-21 00:09:06 +0900

    ⚡️ Improve performance

--- bash/completions/mix.sh
+++ bash/completions/mix.sh
@@ -3,42 +3,35 @@
 # code from https://github.com/dalexj/mix_autocomplete
 
 _mix() {
-  if ! type mix &>/dev/null || ! [ -e mix.exs ]; then
-    return
-  fi
+  declare -g _last_mix_md5
+  declare -g _mix_task_list
+  declare current_mix_md5
 
-  local current_mix_md5
-
-  if type "md5" &>/dev/null; then
-    current_mix_md5=$(md5 -q mix.exs)
-  elif type "md5sum" &>/dev/null; then
-    current_mix_md5=$(md5sum mix.exs | awk '{ print $1 }')
-  else
-    # no either md5 or md5sum
+  if ! type mix &>/dev/null; then
     return 1
   fi
 
-  if [[ $current_mix_md5 != $_last_mix_md5 ]]; then
-    mix compile >/dev/null 2>&1
-    if [[ $? -eq 1 ]]; then
-      # Project compilation failed --
-      # show a message explaining why we can't show completions?
-      return 1
-    fi
-    _last_mix_md5=$current_mix_md5
-    _mix_task_list="$(
-      mix run -e '
-        Mix.Task.load_all
-          |> Enum.map(&(Mix.Task.task_name &1))
-          |> Enum.sort
-          |> Enum.join(" ")
-          |> IO.puts
-      '
-    )"
+  current_mix_md5=$(_mix_caculate)
+  if [[ "$current_mix_md5" != "$_last_mix_md5" ]]; then
+    _mix_task_list="$(mix help --names | xargs echo)"
   fi
 
-  cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=($(compgen -W "$_mix_task_list" $cur))
+  _last_mix_md5=$current_mix_md5
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  IFS=" " read -r -a COMPREPLY <<< "$(compgen -W "$_mix_task_list" "$cur" | xargs echo)"
   return 0
 }
+
+_mix_caculate() {
+  if ! [ -e mix.exs ]; then
+    asdf current elixir
+  elif type "md5" &>/dev/null; then
+    md5 -q mix.exs
+  elif type "md5sum" &>/dev/null; then
+    md5sum mix.exs | awk '{ print $1 }'
+  else
+    echo ""
+  fi
+}
+
 complete -o bashdefault -o default -o nospace -F _mix mix
