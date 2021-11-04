8bb04e4c0d523f4e791c1d7b3253ec9c576f08c4
marocchino <marocchino@users.noreply.github.com>
2021-11-05 08:56:38 +0900

    ⚡️ use lint with wrapper

--- nvim/lua/lint-config.lua
+++ nvim/lua/lint-config.lua
@@ -1,7 +1,7 @@
 require("lint").linters.rubocop = {
-  cmd = "rubocop-daemon",
+  cmd = "rubocop-daemon-wrapper",
   stdin = false, -- or false if it doesn't support content input via stdin. In that case the filename is automatically added to the arguments.
-  args = { "exec", "%", "--", "--format", "json" }, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
+  args = { "--format", "json" }, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
   stream = nil, -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
   ignore_exitcode = false, -- set this to true if the linter exits with a code != 0 and that's considered normal.
   env = nil, -- custom environment table to use with the external process. Note that this replaces the *entire* environment, it is not additive.
