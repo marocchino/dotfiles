6ee1af5bd3f6d395abfd787be9c9ce6df5085880
marocchino <marocchino@users.noreply.github.com>
2023-01-10 09:19:27 +0900

    ⬆️ Update nvim-tree.lua repo

--- nvim/lua/packer-config.lua
+++ nvim/lua/packer-config.lua
@@ -243,7 +243,7 @@ return require("packer").startup(function()
     end,
   })
   use({
-    "kyazdani42/nvim-tree.lua",
+    "nvim-tree/nvim-tree.lua",
     requires = { "kyazdani42/nvim-web-devicons" },
     config = function()
       pcall(require, "tree-config")
