30a7d824e765ff47f322727e0f78be7d5d388a0c
marocchino <marocchino@users.noreply.github.com>
2023-02-23 20:47:02 +0900

    🔧 Remove tab support

--- nvim/lua/cmp-config.lua
+++ nvim/lua/cmp-config.lua
@@ -9,9 +9,6 @@ cmp.setup({
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<C-S-f>"] = cmp.mapping.scroll_docs( -4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
