8940b005fc08395a4c056fa8a1705674f3a21a20
marocchino <marocchino@users.noreply.github.com>
2023-04-16 03:21:33 +0900

    ⬆️ Update deps

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -178,11 +178,11 @@ require("lazy").setup({
         init = function()
           -- Require providers
           require("hover.providers.lsp")
-          -- require("hover.providers.gh")
+          require("hover.providers.gh")
           -- require('hover.providers.gh_user')
           -- require('hover.providers.jira')
           -- require("hover.providers.man")
-          -- require("hover.providers.dictionary")
+          require("hover.providers.dictionary")
         end,
         preview_opts = {
           border = nil,
