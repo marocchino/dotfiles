b15de3b060a0b2c9d3a5f8a622b84c3d8a53c347
marocchino <marocchino@users.noreply.github.com>
2023-04-25 04:37:15 +0900

    🔥 Disable lua-ls formatting

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -8,7 +8,7 @@ local lsp_formatting = function(bufnr)
   vim.lsp.buf.format({
     filter = function(client)
       -- apply whatever logic you want (in this example, we'll only use null-ls)
-      return client.name == "null-ls" or client.name == "lua_ls"
+      return client.name == "null-ls"
     end,
     bufnr = bufnr,
   })
